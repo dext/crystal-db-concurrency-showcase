7f8836e2ed2583c7fa926c84e505a1041b397df7
Yassen Damyanov <yd@itlabs.bg>
2023-03-23 13:58:32 +0200

    fixup! Baseline commit for crystal concurrent db showcase
    Add forgotten fix to postgres DDL

--- src/model/on_postgres.cr
+++ src/model/on_postgres.cr
@@ -7,13 +7,13 @@ module Showcase
       statement = <<-SQL
       CREATE TABLE IF NOT EXISTS employees (
         id SERIAL PRIMARY KEY,
-        team_id INT NULL,
+        team_id INT NOT NULL,
         name VARCHAR(20) NOT NULL
       );
 
       CREATE TABLE IF NOT EXISTS mentors (
         id SERIAL PRIMARY KEY,
-        team_id INT NULL,
+        team_id INT NOT NULL,
         employee_id INT NOT NULL,
         name VARCHAR(20) NOT NULL
       );
